4bec6145b866af7020b51b984245f0d57f36744f
NAGARRO\lalitrana <lalit.rana@nagarro.com>
2022-05-01 17:08:41 +0530

    read data from cosmosDB

---
+++ azure-cosmos-db-graph-nodejs-getting-started/config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.endpoint = "wss://lalit-rana.gremlin.cosmos.azure.com:443/";
+config.primaryKey = "2zdIj99gZgyuXHks61V7VPNCbvsPD4jtNG6z2ezwQzqBydoQo6qmQGHsKUHHQIWWByuBSl97PIByLzRydbe1rg==";
+config.database = "sample-database"
+config.collection = "sample-graph"
+
+module.exports = config;
